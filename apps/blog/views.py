fa87a94c55042b46b6dc796b5d49aff80b70db31
Jorge Agustin Loyola <agusloyola21@gmail.com>
2025-05-15 18:39:20 -0300

    hicimos las vista para listar los post de blog

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -5,6 +5,7 @@ from rest_framework import status, permissions
 from .models import Post, ViewCount
 from apps.category.models import Category
 from .serializers import PostSerializer, PostListSerializer
+from .pagination import SmallSetPagination,LargeSetPagination,MediumSetPagination
 
 
 class BlogListViews(APIView):
@@ -13,10 +14,27 @@ class BlogListViews(APIView):
     def get(self, request, format=None):
         if Post.objects.all().exists():
             post = Post.objects.all()
-            serilizer = PostListSerializer(post, many=True)
+
+            paginator = SmallSetPagination()
+            result = paginator.paginate_queryset(post, request)
+            serilizer = PostListSerializer(result, many=True)
+            
 
             print("LIST POST")
-            return Response({"post": serilizer.data}, status=status.HTTP_200_OK)
+            return paginator.get_paginated_response({"post": serilizer.data})
             
         else:
             return Response({'error':'No post found'}, status=status.HTTP_404_NOT_FOUND)
+        
+class ListPostByCategory(APIView):
+    permission_classes = (permissions.AllowAny,)
+    
+    def get(self, request, format=None):
+        if Post.objects.all().exists():
+            post = Post.objects.all().order_by('-published')
+            print("ListPostByCategory")
+            return Response({'success': 'test ListPostByCategory'}, status=status.HTTP_200_OK )
+        else:
+            return Response({'error':'No post found'}, status=status.HTTP_404_NOT_FOUND)
+            
+        
